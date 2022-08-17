420690678c6010cd4bf2f566e5369961bde14a57
h62j8n <h62j8n@gmail.com>
2022-08-17 15:33:52 +0900

    기본 리소스 추가

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "./plugins/vue-fontawesome";
+import "typeface-notosans-kor";
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .component("fa-fa", FontAwesomeIcon)
+  .mount("#app");
